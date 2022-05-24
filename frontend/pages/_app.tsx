e6410bd83fab2e199e473c3af4b80c7019240a1f
Lan Phan <lanc_phan@hotmail.com>
2022-05-24 13:25:49 -0400

    creating a page layout component

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,8 +1,11 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Page from '../components/layout/Page'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <Page>
+      <Component {...pageProps} />
+    </Page >
+  )
 }
 
-export default MyApp
