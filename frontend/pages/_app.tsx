c5fd75d6054d96ce2a69c261d01c47ffd127cb1c
Lan Phan <lanc_phan@hotmail.com>
2022-05-24 21:26:07 -0400

    installed 'nprogress' and used custom _app file

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,5 +1,12 @@
-import type { AppProps } from 'next/app'
 import Page from '../components/layout/Page'
+import NProgress from 'nprogress'
+import Router from 'next/router'
+import '../components/styles/nprogress.css'
+import { AppProps } from 'next/app'
+
+Router.events.on('routeChangeStart', () => NProgress.start())
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
