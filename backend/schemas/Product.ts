815ef34e89997ba5cacb00ac14a92542136c5e05
Lan Phan <lanc_phan@hotmail.com>
2022-05-25 23:28:16 -0400

    create our Products data type

---
+++ backend/schemas/Product.ts
@@ -0,0 +1,25 @@
+import { list } from '@keystone-6/core'
+import { integer, select, text } from '@keystone-6/core/fields'
+
+export const Product = list({
+  // TODO
+  // access
+  fields: {
+    name: text({ validation: { isRequired: true } }),
+    description: text({ ui: { displayMode: 'textarea' } }),
+    status: select({
+      options: [
+        { label: 'Draft', value: 'DRAFT' },
+        { label: 'Available', value: 'AVAILABLE' },
+        { label: 'Unavailable', value: 'UNAVAILABLE' },
+      ],
+      defaultValue: 'DRAFT',
+      ui: {
+        displayMode: 'segmented-control',
+        // createView: { fieldMode: 'hidden' },
+      },
+    }),
+    price: integer(),
+    // TODO: photo
+  },
+})
