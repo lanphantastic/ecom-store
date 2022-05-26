1f3edf97fd1a5f4772be32eb539997635353cea5
Lan Phan <lanc_phan@hotmail.com>
2022-05-25 23:13:18 -0400

    cleaning the keystone config

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -1,41 +1,3 @@
-import 'dotenv/config'
-// import {config} from '@keystone-6/core'
-// import { KeystoneConfig, DatabaseConfig, AdminUIConfig } from '@keystone-6/core/types'
-
-const databaseURL = process.env.DATABASE_URL || 'postgres://dbuser:dbpass@localhost:5432/keystone'
-
-// export default config({
-//   db: {
-//     provider: 'postgresql',
-//     url: databaseURL,
-//   },
-//   lists: {
-
-//   },
-//   ui: {
-//     // TODO: change this for rules
-//     isAccessAllowed: () => true
-//   }
-// })
-// export default config({
-// server: {
-//   cors: {
-//     origin: [process.env.FRONTEND_URL || 'localhost'],
-//     credentials: true,
-//   },
-// },
-// db: {
-//   provider: 'postgresql',
-//   url: databaseURL,
-//   // TODO: add data seeding here
-// },
-
-// })
-
-// const sessionConfig = {
-//   maxAge: 60 * 60 * 24 * 360, // how long should they stay signed in?
-//   secret: process.env.COOKIE_SECRET
-// }
 /*
 Welcome to Keystone! This file is what keystone uses to start the app.
 
@@ -43,6 +5,9 @@ It looks at the default export, and expects a Keystone config object.
 
 You can find all the config options in our docs here: https://keystonejs.com/docs/apis/config
 */
+
+import 'dotenv/config'
+const databaseURL = process.env.DATABASE_URL || 'postgres://dbuser:dbpass@localhost:5432/keystone'
 
 import { config } from '@keystone-6/core'
 
