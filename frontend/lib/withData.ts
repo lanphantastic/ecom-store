117eaa9a96e7031e885e40a6163a923342c08aeb
Lan Phan <lanc_phan@hotmail.com>
2022-05-28 14:58:19 -0400

    get the apollo client working

--- frontend/lib/withData.ts
+++ frontend/lib/withData.ts
@@ -5,43 +5,37 @@ import { createUploadLink } from 'apollo-upload-client'
 import withApollo from 'next-with-apollo'
 import { endpoint, prodEndpoint } from '../config'
 
-function createClient({ headers, initialState }: any): ApolloClient<NormalizedCacheObject> {
-  const cache = new InMemoryCache({
-    typePolicies: {
-      Query: {
-        fields: {
-          // TODO: We will add this together!
-          // allProducts: paginationField(),
+let apolloClient: ApolloClient<NormalizedCacheObject> | undefined
+
+function createClient({ headers, initialState }: any) {
+  return new ApolloClient({
+    link: ApolloLink.from([
+      onError(({ graphQLErrors, networkError }) => {
+        if (graphQLErrors)
+          graphQLErrors.forEach(({ message, locations, path }) =>
+            console.log(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`),
+          )
+        if (networkError) console.log(`[Network error]: ${networkError}. Backend is unreachable. Is it running?`)
+      }),
+      // this uses apollo-link-http under the hood, so all the options here come from that package
+      createUploadLink({
+        uri: process.env.NODE_ENV === 'development' ? endpoint : prodEndpoint,
+        fetchOptions: {
+          credentials: 'include',
+        },
+        // pass the headers along from this request. This enables SSR with logged in state
+        headers,
+      }),
+    ]),
+    cache: new InMemoryCache({
+      typePolicies: {
+        Query: {
+          fields: {
+            // TODO: We will add this together!
+          },
         },
       },
-    },
-  }).restore(initialState || {})
-
-  const link = ApolloLink.from([
-    // returns either a GraphQL error or a network error
-    onError(({ graphQLErrors, networkError }) => {
-      if (graphQLErrors)
-        graphQLErrors.forEach(({ message, locations, path }) =>
-          console.log(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`),
-        )
-      if (networkError) console.log(`[Network error]: ${networkError}. Backend is unreachable. Is it running?`)
-    }),
-    // this uses apollo-link-http under the hood, so all the options here come from that package
-    createUploadLink({
-      uri: process.env.NODE_ENV === 'development' ? endpoint : prodEndpoint,
-      fetchOptions: {
-        credentials: 'include',
-      },
-      // pass the headers along from this request. This enables SSR with logged in state
-      headers,
-    }),
-  ])
-
-  return new ApolloClient({
-    link,
-    cache,
-    name: 'ecomm',
-    version: '1.0'
+    }).restore(initialState || {}),
   })
 }
 
